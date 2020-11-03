4c7be7a207e032a692b84349b4c16a2aa3cb71fc
jagathaikhan <babak.ilya.official@gmail.com>
2020-11-03 16:18:26 +0300

    Updated

--- defines.php
+++ defines.php
@@ -3,5 +3,5 @@
 	
 	define( 'INSTAGRAM_APP_ID', '329083761721472');
 	define( 'INSTAGRAM_APP_SECRET', 'bc08476b4130fa2eb060e1542475a600');
-	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://jagathaikhan.github.io/bstu-project/');
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://intense-sea-88814.herokuapp.com/');
 ?>
