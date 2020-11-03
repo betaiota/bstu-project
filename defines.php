37630752b1d2918d703d68bec63f06cf9e663bbb
jagathaikhan <babak.ilya.official@gmail.com>
2020-11-03 16:06:57 +0300

    Updated

--- defines.php
+++ defines.php
@@ -1,5 +1,5 @@
 <?php
-	require_once '../vendor/autoload.php';
+	require_once 'vendor/autoload.php';
 	
 	define( 'INSTAGRAM_APP_ID', '329083761721472');
 	define( 'INSTAGRAM_APP_SECRET', 'bc08476b4130fa2eb060e1542475a600');
