38e90f6cd3e2ba8df8b2764a5cbdf1e5a365a22e
jagathaikhan <babak.ilya.official@gmail.com>
2020-11-03 19:37:41 +0300

    Посты

--- index.php
+++ index.php
@@ -27,6 +27,33 @@
 	<h4>Raw Data</h4>
 	<textarea style="width:100%;height:400px;"><?php print_r( $usersMedia ); ?></textarea>
 	<h4>Posts</h4>
+	<ul style="list-style: none;margin:0px;padding:0px;">
+		<?php foreach ( $usersMedia['data'] as $post ) : ?>
+			<li style="margin-bottom:20px;border:3px solid #333">
+				<div>
+					<?php if ( 'IMAGE' == $post['media_type'] || 'CAROUSEL_ALBUM' == $post['media_type']) : ?>
+						<img style="height:320px" src="<?php echo $post['media_url']; ?>" />
+					<?php else : ?>
+						<video height="240" width="320" controls>
+							<source src="<?php echo $post['media_url']; ?>">
+						</video>
+					<?php endif; ?>
+				</div>
+				<div>
+					<b>Caption: <?php echo $post['caption']; ?></b>
+				</div>
+				<div>
+					ID: <?php echo $post['id']; ?>
+				</div>
+				<div>
+					Media Type: <?php echo $post['media_type']; ?>
+				</div>
+				<div>
+					Media URL: <?php echo $post['media_url']; ?>
+				</div>
+			</li>
+		<?php endforeach; ?>
+	</ul>
 <?php else : ?>
 	<a href="<?php echo $ig->authorizationUrl; ?>">
 		Авторизация
